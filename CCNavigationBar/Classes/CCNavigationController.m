a15f6bae3bd8f33e73d351767060ee494b22044b
Charles Wang <charles.wang@skytv.co.nz>
2018-01-16 11:51:42 +1300

    upgrade CCNavigationController

--- CCNavigationBar/Classes/CCNavigationController.m
+++ CCNavigationBar/Classes/CCNavigationController.m
@@ -26,20 +26,10 @@
     self.delegate = (id<UINavigationControllerDelegate>) self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.topViewController updateNavigationBarViewFrame];
 }
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
 - (void)setViewControllers:(NSArray<__kindof UIViewController *> *)viewControllers
 {
@@ -64,4 +54,3 @@
 }
 
 @end
-
